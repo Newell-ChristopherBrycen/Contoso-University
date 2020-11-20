550b3805ea64bd4a9334724548e537dcbade170f
Christopher Newell <cbnewell@byui.edu>
2020-11-19 23:06:36 -0600

    Up to Advanced Topics

---
+++ Migrations/20201120050217_Inheritance.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class Inheritance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseAssignment_Instructors_InstructorID",
+                table: "CourseAssignment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Department_Instructors_InstructorID",
+                table: "Department");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollment_Students_StudentID",
+                table: "Enrollment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OfficeAssignment_Instructors_InstructorID",
+                table: "OfficeAssignment");
+
+            migrationBuilder.DropTable(
+                name: "Instructors");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Students",
+                table: "Students");
+
+            migrationBuilder.RenameTable(
+                name: "Students",
+                newName: "Person");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EnrollmentDate",
+                table: "Person",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "HireDate",
+                table: "Person",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Person",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Person",
+                table: "Person",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseAssignment_Person_InstructorID",
+                table: "CourseAssignment",
+                column: "InstructorID",
+                principalTable: "Person",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Department_Person_InstructorID",
+                table: "Department",
+                column: "InstructorID",
+                principalTable: "Person",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollment_Person_StudentID",
+                table: "Enrollment",
+                column: "StudentID",
+                principalTable: "Person",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OfficeAssignment_Person_InstructorID",
+                table: "OfficeAssignment",
+                column: "InstructorID",
+                principalTable: "Person",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseAssignment_Person_InstructorID",
+                table: "CourseAssignment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Department_Person_InstructorID",
+                table: "Department");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollment_Person_StudentID",
+                table: "Enrollment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OfficeAssignment_Person_InstructorID",
+                table: "OfficeAssignment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Person",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "HireDate",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Person");
+
+            migrationBuilder.RenameTable(
+                name: "Person",
+                newName: "Students");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EnrollmentDate",
+                table: "Students",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Students",
+                table: "Students",
+                column: "ID");
+
+            migrationBuilder.CreateTable(
+                name: "Instructors",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    HireDate = table.Column<DateTime>(nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instructors", x => x.ID);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseAssignment_Instructors_InstructorID",
+                table: "CourseAssignment",
+                column: "InstructorID",
+                principalTable: "Instructors",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Department_Instructors_InstructorID",
+                table: "Department",
+                column: "InstructorID",
+                principalTable: "Instructors",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollment_Students_StudentID",
+                table: "Enrollment",
+                column: "StudentID",
+                principalTable: "Students",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OfficeAssignment_Instructors_InstructorID",
+                table: "OfficeAssignment",
+                column: "InstructorID",
+                principalTable: "Instructors",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
